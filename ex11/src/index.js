79abb0bbcca4b482cbdee6ea453824ade4690dbb
nedimpanjeta <panjeta.nedim@gmail.com>
2021-03-16 22:31:04 +0100

    Day09

---
+++ ex11/src/index.js
@@ -0,0 +1,35 @@
+// Only change code below this line 
+function switchCase(letter) {
+    var animals = letter;
+
+    switch (animals) {
+        case "a":
+            animals = "antelope";
+            break;
+
+        case "b":
+           animals = "bird";
+            break;
+
+        case "c":
+           animals = "cat";
+            break;
+
+        default:
+            animals = "stuff";
+            break;
+
+    }
+
+
+    // Only change code above this line 
+    return animals;
+}
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("default"));
+
+
+
+module.exports = switchCase;
---
+++ ex13/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line 
+function inverseWhile() {
+    var fiveNumbers = 5;
+    while (fiveNumbers >= 0) {
+        fiveNumbers--;
+        
+        
+        
+    }
+    return "5,4,3,2,1,0";
+}
+
+    // Only change code above this line 
+ 
+
+console.log(inverseWhile());
+
+module.exports = inverseWhile;
